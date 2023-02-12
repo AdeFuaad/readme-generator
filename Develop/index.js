7910ff7ef56d43d1c976358c40742aae9917a6c6
AdeFuaad <adesholafuaad@gmail.com>
2023-02-12 16:08:37 -0500

    added the init function

--- Develop/index.js
+++ Develop/index.js
@@ -1,4 +1,8 @@
 // TODO: Include packages needed for this application
+const fs = require('fs');
+const inquirer = require('inquirer');
+const generateMarkdown = require('./utils/generateMarkdown');
+
 // TODO: Create an array of questions for user input
 const questions = 
 inquirer
@@ -42,11 +46,13 @@ inquirer
     type: 'list',
     message: 'Select your license from the list?',
     name: 'license',
-    choices: ['License1', 'License2', 'License3']
+    choices: ['No license', 'Apache License 2.0', 'MIT License', 'GNU General Public License v3.0', 
+    "BSD 2-Clause 'Simplified' License", 'Boost Software License 1.0', 'Mozilla Public License 2.0', 'The Unlicense',
+    'GNU General Public License v2.0', 'GNU Lesser General Public License v3.0']
     },
     {
     type: 'input',
-    message: 'What is your github URL',
+    message: 'What is your github username',
     name: 'github',
     },
     {
@@ -62,87 +68,24 @@ inquirer
 ])
 // emptyInput ()
 
-.then ((response) => {
-    const fileName = 'readme.me';
-    const {name, description, installation, usage, contribution, features, test, license, linkedin, github, email} = data;
-
-    const generateREADME = 
-
-    `# ${name}
-
-    LineLogic is a website that pulls data from the YouTube API and GitHub API using the Tailwind CSS framework. It is designed to be responsive, providing a seamless user experience on different devices.
-    
-    ## Description
-
-    ${description}
-
-
-    ## Table of Contents
-    
-
-    - [Installation](#installation)
-    - [Usage](#usage)
-    - [Credits](#credits)
-    - [License](#license)
-    - [Features](#features)
-    - [License](#license)
-
-    ## Installation
-
-    ${installation}
-
-    ## Usage
-
-    ${usage}
-
-    ![Displays The Youtube Videos and Github Repository Desktop Responsive"LineLogic".](./assets/images/LineLogic.png)
-
-    ## Questions
-
-    Here's my github username: ${github}
-    Here's my linkedin username: ${linkedin}
-    Here's my email address: http://mailto:${email}
-
-    ## License
-
-    ${license}
-
-    ## Features
-
-    ${features}
-
-    ## How to Contribute
-
-    ${contribution}
-
-    ## Tests
-
-    ${test}
-
-    `
-}
-);
+// .then ((response) => {
+//     const fileName = 'readme.me';
+//     const {name, description, installation, usage, contribution, features, test, license, linkedin, github, email} = data;
+// }
+// );
 
 function writeToFile(fileName, data) {
-    const fs = require('fs');
-    fs.writeFile(fileName, data, (err) =>
-        err ? console.error(err) : console.log('Success!')
-    )
+    let rmMarkDown = generateMarkdown(data);
+    fs.appendFile(fileName, rmMarkDown, (err) =>
+      err ? console.error(err) : console.log('Success!')
+    );
 }
 // TODO: Create a function to initialize app
 function init() {
-    arrayOfQuestions()
-}
-
-function init() {
-    const fs = require('fs');
-    const inquirer = require('inquirer');
-
     inquirer
     .prompt(questions)
-    .then((answers) => {
-        writeToFile("./samples/json/readme.json", JSON.stringify(answers))
-        writeToFile("./samples/readme/readme.md", generateMarkdown(answers));
+    .then((response) => {
+        writeToFile('userReadMe.md', response);
     }).catch();
 }
 // Function call to initialize app
@@ -151,3 +94,9 @@ init();
 // http://github.com/adefuaad/
 // http://linkdein.com/in/fuaad-shobambi/
 // adesholafuaad@gmail.com
+
+
+
+
+
+
